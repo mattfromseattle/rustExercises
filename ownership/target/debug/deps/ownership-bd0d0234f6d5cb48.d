cf42a1c1a2bb597cea071434b57487835fa0fe43
Matt Dewey <matthew.dewey@47deg.com>
2019-12-31 14:26:18 -0800

    Ownership

---
+++ ownership/target/debug/deps/ownership-bd0d0234f6d5cb48.d
@@ -0,0 +1,5 @@
+/Users/matthewdewey/Development/rustExercises/ownership/target/debug/deps/ownership-bd0d0234f6d5cb48: src/main.rs
+
+/Users/matthewdewey/Development/rustExercises/ownership/target/debug/deps/ownership-bd0d0234f6d5cb48.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/matthewdewey/Development/rustExercises/ownership/target/debug/ownership: /Users/matthewdewey/Development/rustExercises/ownership/src/main.rs
